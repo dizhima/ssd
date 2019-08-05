ae41ef5c9cd7ff2fc7f1356ee745fb94d750d114
DM <dizhim@uw.edu>
2019-08-04 21:35:43 -0700

    customized

--- ssd/utils/box_utils.py
+++ ssd/utils/box_utils.py
@@ -85,7 +85,7 @@ def assign_priors(gt_boxes, gt_labels, corner_form_priors,
     """
     # size: num_priors x num_targets
     try:ious = iou_of(gt_boxes.unsqueeze(0), corner_form_priors.unsqueeze(1))
-    except:print(ious)
+    except RuntimeError:print(ious)
     # size: num_priors
     best_target_per_prior, best_target_per_prior_index = ious.max(1)
     # size: num_targets
